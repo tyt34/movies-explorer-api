7bcac826cb25bb9eb70410e347dd74791fd27485
R N <novoto4in@mail.ru>
2021-11-24 22:24:39 +0300

    0.05

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,33 @@ const NotFoundError = require('../errors/not-found-error');
 const CastError = require('../errors/cast-error');
 const ValidationError = require('../errors/validation-error');
 const WrongKeys = require('../errors/wrong-keys');
+const RepeatEmail = require('../errors/repeat-email');
+const NotNewInfo = require('../errors/not-new-info');
+
+function updateNewInfo(name, email, id, res, next) {
+  User.findByIdAndUpdate(
+    {
+      _id: id,
+    },
+    {
+      name,
+      email,
+    },
+    {
+      new: true,
+      runValidators: true,
+    },
+  )
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError();
+      }
+      return res.status(200).send(user);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
 
 module.exports.getUser = (req, res, next) => {
   User.findById(req.user._id)
@@ -19,38 +46,40 @@ module.exports.getUser = (req, res, next) => {
 };
 
 module.exports.updateUser = (req, res, next) => {
-  console.log(' update ');
   const { name, email } = req.body;
   if ((name === undefined) && (email === undefined)) {
     throw new WrongKeys();
   }
-  User.findByIdAndUpdate(
-    {
-      _id: req.user._id,
-    },
-    {
-      name,
-      email,
-    },
-    {
-      new: true,
-      runValidators: true,
-    },
-  )
-    .then((user) => {
-      console.log(' -> -> ');
-      console.log(user)
-      return user
-    })
-    .then((user) => {
-      console.log(' -> ');
-      if (!user) {
-        throw new NotFoundError();
+  User.findOne({ email })
+    .then((info) => {
+      if (info === null) {
+        updateNewInfo(name, email, req.user._id, res, next);
+      } else {
+        /*
+        if ((req.user._id !== info._id.toString())) {
+          next(new RepeatEmail());
+        } else {
+          if ((info.name === name) && (info.email === email)) {
+            throw new NotNewInfo();
+          } else if (info.email === email) {
+            updateNewInfo(name, email, req.user._id, res, next);
+          } else {
+            next(new RepeatEmail());
+          }
+        }
+        */
+        if (req.user._id !== info._id.toString()) {
+          next(new RepeatEmail());
+        } else if ((info.name === name) && (info.email === email)) {
+          throw new NotNewInfo();
+        } else if (info.email === email) {
+          updateNewInfo(name, email, req.user._id, res, next);
+        } else {
+          next(new RepeatEmail());
+        }
       }
-      return res.status(200).send(user);
     })
     .catch((err) => {
-      console.log(err.name);
       if (err.name === 'ValidationError') next(new ValidationError());
       if (err.name === 'NotFoundError') next(new NotFoundError());
       next(err);
